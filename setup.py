73ea8c097f58596d634a5a323e4139c343adc0f7
ebbkrdrcn <ebubekirdirican@gmail.com>
2017-04-14 03:15:04 +0300

    Version changed.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='RexP',
-    version='0.0.2',
+    version='0.0.3',
     packages=['rexp'],
     description='Simple pattern compiler library',
     author='Ebubekir Dirican',
